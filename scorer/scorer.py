d1164638fd6856d44ffcc35b233666f9a9622aba
John <johncollins.ie@gmail.com>
2026-04-14 15:50:38 -0600

    setup

---
+++ scorer/scorer.py
@@ -0,0 +1,5 @@
+def main():
+    print("Scorer running")
+
+if __name__ == "__main__":
+    main()
---
+++ simulator/simulator.py
@@ -0,0 +1,5 @@
+def main():
+    print("Simulator running")
+
+if __name__ == "__main__":
+    main()
